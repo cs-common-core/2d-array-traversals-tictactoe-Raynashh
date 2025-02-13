008b522ea16bfff6e4f17a01cfd95052ee0be605
Anray Sheng <asheng27@nobles.edu>
2025-02-13 16:43:02 +0000

    tictactoe commit

--- src/Play.java
+++ src/Play.java
@@ -6,9 +6,17 @@ public class Play {
         System.out.println("Player 1 is X, Player 2 is O");
         System.out.println("Enter the row and column to play");
         TicTacToe game = new TicTacToe();
+        
         while(!game.isGameOver()) {
+            System.out.println("It is Player " + game.getCurrentPlayer() + "'s turn.");
             game.printBoard();
             // your code here
+            System.out.println("Row: ");
+            int row = scanner.nextInt();
+            System.out.println("Column: ");
+            int col = scanner.nextInt();
+            game.play(row, col);
+
         }
         scanner.close();
     }
--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -8,11 +8,15 @@ public class TicTacToe {
     }
 
     public void play(int row, int col) {
-        if(board[row][col] != 0) {
+        if(row < 1 || row > 3 || col < 1 || col >3){
+            System.out.println("Invalid row / column. Please try again.");
+            return;
+        }
+        if(board[row-1][col-1] != 0) {
             System.out.println("Cell already occupied");
             return;
         } else {
-            board[row][col] = currentPlayer;
+            board[row-1][col-1] = currentPlayer;
             if(currentPlayer == 1) {
                 currentPlayer = 2;
             } else {
@@ -21,10 +25,33 @@ public class TicTacToe {
         }
     }
 
+    public void printBoard(){
+        System.out.println("_____________");
+        for(int i = 0; i<3; i++){
+            System.out.print("| ");
+            for(int j = 0; j<3; j++){
+                if(board[i][j]==0){
+                    System.out.print("  ");
+                } else if (board[i][j] == 1){
+                    System.out.print("X ");
+                } else {
+                    System.out.print("O ");
+                }
+                System.out.print("| ");
+            }
+            System.out.println();
+            System.out.println("_____________");
+        }
+    }
+
     public boolean isGameOver() {
         if(getWinner() != 0) {
+            printBoard();
+            System.out.println("Congratulations! Player " + currentPlayer + " has won!");
             return true;
         } else if(isBoardFull()) {
+            printBoard();
+            System.out.println("The game has ended in a draw.");
             return true;
         } else {
             return false;
@@ -39,6 +66,11 @@ public class TicTacToe {
         // check rows
         for(int i = 0; i < 3; i++) {
             if(board[i][0] == board[i][1] && board[i][1] == board[i][2] && board[i][0] != 0) {
+                if(currentPlayer == 1) {
+                    currentPlayer = 2;
+                } else {
+                    currentPlayer = 1;
+                }
                 return currentPlayer;
             }
         }
@@ -69,7 +101,7 @@ public class TicTacToe {
         return true;
     }
 
-    public void printBoard() {
+    /*public void printBoard() {
         for(int i = 0; i < 3; i++) {
             for(int j = 0; j < 3; j++) {
                 if(board[i][j] == 0) {
@@ -82,5 +114,5 @@ public class TicTacToe {
             }
             System.out.println();
         }
-    }
+    }*/
 }
